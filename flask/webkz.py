65032c343e33690af3d5913b54e1a8736da74c28
vonnyfly <lifeng1519@gmail.com>
2016-01-07 00:05:07 +0800

    delete init db

--- flask/webkz.py
+++ flask/webkz.py
@@ -26,21 +26,6 @@ def connect_db():
     return rv
 
 
-def init_db():
-    """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
-
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
